712e302ee05f72068277fd0ff9a75e7ef22ce734
minguri <122717574+rn1nha@users.noreply.github.com>
2023-01-30 20:42:17 +0900

    Add files via upload

---
+++ pythonProject/day06/section12/Ex12-3-module3.py
@@ -0,0 +1,5 @@
+
+from converter import *
+
+miles = kilometer_to_miles(150)
+print('150km = {}miles'.format(miles))
---
+++ pythonProject/day06/section12/Ex12-5-module5.py
@@ -0,0 +1,9 @@
+'''
+별명 사용하기2
+'''
+from converter import kilometer_to_miles as k_to_m
+miles = k_to_m(150)
+print('150km = {}miles'.format(miles))
+
+pounds = k_to_m(1000)
+print('1000g = {}pounds'.format(pounds))
