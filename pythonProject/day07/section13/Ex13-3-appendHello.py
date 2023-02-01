edbde837f906ad4c69ea475aedbd524b4c629a39
minguri <122717574+rn1nha@users.noreply.github.com>
2023-02-01 21:18:51 +0900

    Add files via upload

---
+++ pythonProject/day07/section13/Ex13-3-appendHello.py
@@ -0,0 +1,9 @@
+'''
+
+a (append mode) : 추가 모드
+'''
+file = open('upload/hello.txt', 'at', encoding='UTF-8')
+file.write('Hello\n')
+file.write('Nice to meet you\n')
+print('hello.txt 파일에 새로운 내용이 추가되었습니다.')
+file.close()
