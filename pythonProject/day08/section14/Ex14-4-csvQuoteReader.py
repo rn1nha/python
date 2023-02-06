e1073fff33df9c80b2a60b913c5d3a03933a6ffe
minguri <122717574+rn1nha@users.noreply.github.com>
2023-02-06 20:27:19 +0900

    Add files via upload

--- pythonProject/day08/section14/Ex14-4-csvQuoteReader.py
+++ pythonProject/day08/section14/Ex14-4-csvQuoteReader.py
@@ -5,7 +5,7 @@ member_list = []
 with open('회원명단.csv','rt', encoding='UTF-8') as file:
     file.readline()
     while True:
-        line = file.readline()
+        line = file.readline()   #첫줄제거용
         if not line:
             break
         member = line.split(',')
