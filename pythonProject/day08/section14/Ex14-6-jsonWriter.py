0dfddfc91ed13575d58ccdf885a219842899827b
minguri <122717574+rn1nha@users.noreply.github.com>
2023-02-06 20:52:22 +0900

    Add files via upload

--- pythonProject/day08/section14/Ex14-6-jsonWriter.py
+++ pythonProject/day08/section14/Ex14-6-jsonWriter.py
@@ -6,6 +6,7 @@ JSON (JavaScript Object Notation)
 import json
 
 # 첫번째 방법
+'''
 dict_list =[
     {
 
@@ -29,5 +30,31 @@ dict_list =[
 json_string = json.dumps(dict_list)
 
 with open('dictlist.json', 'w') as file:
+    file.write(json_string)
+print('dictlist.json 파일이 생성되었습니다.')
+'''
+# 두번째 방법
+dict_list =[
+    {
+
+        'name':'james',
+        'age':20,
+        'spec':[
+            175.5,
+            70.5
+        ]
+    },
+    {
+
+        'name':'홍길동',
+        'age':21,
+        'spec': [
+            168.5,
+            60.5
+        ]
+    }
+]
+json_string = json.dumps(dict_list, indent=4, ensure_ascii=False)  #indent 들여쓰기
+with open('dictlist.json', 'w', encoding='UTF-8') as file:
     file.write(json_string)
 print('dictlist.json 파일이 생성되었습니다.')
