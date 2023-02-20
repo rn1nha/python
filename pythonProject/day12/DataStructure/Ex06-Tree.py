fd3575ba3869a4a29639c1922266e8eb7a63910e
minguri <122717574+rn1nha@users.noreply.github.com>
2023-02-20 21:39:18 +0900

    Add files via upload

--- pythonProject/day12/DataStructure/Ex06-Tree.py
+++ pythonProject/day12/DataStructure/Ex06-Tree.py
@@ -33,4 +33,127 @@ class BinarySearchTree(object):
                 node.right = self._insert_value(node.right, data)
         return node
 
+    def find(self, key): # 이진 트리에서 데이터를 찾는 부분
+        return self._find_value(self.root,key)
 
+    def _find_value(self,root,key):
+        if root is None or root.data == key:
+            return root is not None
+        elif key < root.data:
+            return self._find_value(root.left,key)
+        else:
+            return self._find_value(root.right,key)
+
+    def delete(self, key): #이진 트리에서 데이터를 지우는 부분
+        self.root, deleted = self._delete_value(self.root, key)
+        return deleted
+
+    def _delete_value(self,node,key):
+        if node is None:
+            return node, False
+
+        deleted = False
+        if key == node.data:
+            deleted = True
+            if node.left and node.right:
+                parent, child = node, node.right
+                while child.left is not None:
+                    parent, child = node, node.left
+                child.left = node.left
+                if parent != node:
+                    parent.left = child.right
+                    child.right = node.right
+                node = child
+            elif node.left or node.right:
+                node = node.left or node.right
+            else:
+                node = None
+        elif key < node.data:
+            node.left, deleted = self._delete_value(node.left, key)
+        else:
+            node.right._deleted = self._delete_value(node.right, key)
+        return node, deleted
+
+    def DFTravel(self): #데이터를 출력하는 부분
+        """
+
+        깊이 우선 탐색 중 전위 순회
+        뿌리 > 왼쪽 트리 > 오른쪽 트리
+
+        """
+        def _DFTravel(root):
+            if root is None:
+                pass
+            else:
+                print(root.data, end=' ')
+                _DFTravel(root.left)
+                _DFTravel(root.right)
+        _DFTravel(self.root)
+
+    def LFTravel(self):
+        """
+           깊이 우선 탐색 중 중위 순회
+           왼쪽 트리 > 뿌리 > 오른쪽 트리
+        """
+        def _LFTravel(root):
+            if root is None:
+                pass
+            else:
+                _LFTravel(root.left)
+                print(root.data, end= ' ')
+                _LFTravel(root.right)
+        _LFTravel(self.root)
+
+    def LRTravel(self):
+        """
+          깊이 우선 탐색 중 후위 순위
+          왼쪽 트리 > 오른쪽 트리 > 뿌리 노드
+
+        """
+        def _LRTravel(root):
+            if root is None:
+                pass
+            else:
+                _LRTravel(root.left)
+                _LRTravel(root.right)
+                print(root.data, end=' ')
+        _LRTravel(self.root)
+
+    def layerTravel(self):
+        """
+          너비 우선 탐색
+        뿌리 노드부터 깊이 순으로 방문
+        """
+        def _layerTravel(root):
+            queue = [root]
+            while queue:
+                root = queue.pop(0)
+                if root is not None:
+                    print(root.data, end= ' ')
+                    if root.left:
+                        queue.append(root.left)
+                    if root.right:
+                        queue.append(root.right)
+        _layerTravel(self.root)
+
+
+data = [20, 6, 8, 12, 78, 32, 65, 7, 9]
+tree = BinarySearchTree()
+
+for x in data:
+     tree.insert(x)
+
+print('===DFTravel===')
+tree.DFTravel()  # 전위 순회
+print()
+
+print('===LfTravel===')
+tree.LFTravel()  # 중위 순회
+print()
+
+print('===_LRTravel===')
+tree.LRTravel()  # 후위 순회
+print()
+
+print('===layerTravel===')
+tree.layerTravel()  # 너비 우선 탐색
